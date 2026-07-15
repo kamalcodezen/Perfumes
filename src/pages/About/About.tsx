acfd1d54bbb5f3d1325f656e473873ddd19f9f67
kamalcodezen <kamaluddin7908@gmail.com>
2026-07-15 15:47:24 +0530

    feat: add AOS fade-up entrance animation to About page layout

--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -1,19 +1,28 @@
+import { useEffect } from "react";
+import AOS from "aos";
+import "aos/dist/aos.css";
+
 import AboutHero from "../../components/about/AboutHero/AboutHero";
 import BrandStory from "../../components/about/BrandStory/BrandStory";
 import OurMission from "../../components/about/OurMission/OurMission";
 import OurValues from "../../components/about/OurValues/OurValues";
 
 const About = () => {
+  useEffect(() => {
+    AOS.init({
+      duration: 1000,
+      once: true,
+      easing: "ease-out-cubic",
+    });
+  }, []);
+
   return (
-    <>
+    <div className="overflow-x-hidden w-full min-h-screen" data-aos="fade-up">
       <AboutHero />
-
       <BrandStory />
-
       <OurMission />
-
       <OurValues />
-    </>
+    </div>
   );
 };
 
